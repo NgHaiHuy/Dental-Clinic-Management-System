1a5c7702e1d2882f44a8dee8d2fcfb132dda4f87
hamnhdung <teo11122003@gmail.com>
2026-07-06 13:47:58 +0700

    update model appointment
    Update attribute, constructor, getter, setter for Appointment

--- src/java/model/Appointment.java
+++ src/java/model/Appointment.java
@@ -4,10 +4,108 @@
  */
 package model;
 
+import java.util.Date;
+import java.sql.Time;
+
 /**
  *
  * @author Nguye
  */
 public class Appointment {
-    
+    private int appointmentID;
+    private int customerID;
+    private Integer doctorID; // Có thể NULL (Nếu khám tổng quát)
+    private Date appointmentDate;
+    private Time appointmentTime;
+    private String status; // Pending, Confirmed, Attended, Cancelled
+    private String notes;
+    private String patientName;
+    private String patientPhone;
+
+    public Appointment() {
+    }
+
+    public Appointment(int appointmentID, int customerID, Integer doctorID, Date appointmentDate, Time appointmentTime, String status, String notes, String patientName, String patientPhone) {
+        this.appointmentID = appointmentID;
+        this.customerID = customerID;
+        this.doctorID = doctorID;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.status = status;
+        this.notes = notes;
+        this.patientName = patientName;
+        this.patientPhone = patientPhone;
+    }
+
+    public int getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(int appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public Integer getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(Integer doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public Date getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(Date appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public Time getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(Time appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientPhone() {
+        return patientPhone;
+    }
+
+    public void setPatientPhone(String patientPhone) {
+        this.patientPhone = patientPhone;
+    }
 }
