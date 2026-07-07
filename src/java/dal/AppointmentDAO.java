ba000add8f37279ac95f0d4a5a0274232f817aae
hamnhdung <teo11122003@gmail.com>
2026-07-07 17:33:56 +0700

    Update AppointmentDAO
    cập nhật pthuc lấy danh sách lịch hẹn chờ của bác sĩ và hàm update trạng thái lịch hẹn

--- src/java/dal/AppointmentDAO.java
+++ src/java/dal/AppointmentDAO.java
@@ -1,10 +1,61 @@
 package dal;
 
 import context.DBContext;
+import java.util.List;
+import model.Appointment;
+import org.apache.tomcat.dbcp.dbcp2.SQLExceptionList;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Data Access Object for Appointments.
  */
 public class AppointmentDAO extends DBContext {
+
     // Basic skeleton.
+    // Lấy danh sách lịch hẹn chờ khám hôm nay của bác sĩ
+    public List<Appointment> getWaitingAppointmentToday(int doctorID) throws SQLException {
+        List<Appointment> list = new ArrayList<>();
+        String sql = "SELECT a.AppoitmentID, a.CustomerID, a.DoctorID, a.AppointmentDate, a.AppointmentTime, a.Status, a.Notes "
+                + "FROM Appointments a"
+                + "JOIN Users u ON a.CustomerID = u.UserID"
+                + "WHERE a.AppointmentDate = CAST(GETDATE() AS DATE)"
+                + "AND a.Status IN ('Pending', 'Confirmed')"
+                + "AND (a.DoctorID = ? OR a.DoctorID IS NULL) "
+                + "ORDER BY a.AppointmentTime ACS";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, doctorID);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Appointment a = new Appointment();
+                    a.setAppointmentID(rs.getInt("AppointmentID"));
+                    a.setCustomerID(rs.getInt("CustomerID"));
+                    a.setDoctorID(rs.getInt("DoctorID"));
+                    a.setAppointmentDate(rs.getDate("AppointmentDate"));
+                    a.setAppointmentTime(rs.getTime("AppointmentTime"));
+                    a.setStatus(rs.getString("Status"));
+                    a.setNotes(rs.getString("Notes"));
+                    a.setPatientName(rs.getString("PatientName"));
+                    a.setPatientPhone(rs.getString("PatientPhone"));
+                    list.add(a);
+                }
+            }
+        } catch (SQLExceptionList e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    // Cập nhật trạng thái lịch hẹn
+    public boolean updateStatus(int appointmentId, String status) {
+        String sql = "UPDATE Appointments SET Status = ? WHERE AppointmentID = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, status);
+            ps.setInt(2, appointmentId);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
 }
