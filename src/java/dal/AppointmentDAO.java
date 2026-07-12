a949a29fa5cf7b450af125260ce7de08ee38170f
hamnhdung <teo11122003@gmail.com>
2026-07-13 03:52:41 +0700

    fix getWaitingAppointmentToday
    fix lỗi SQL của hàm getWaitingAppointmentToday, tạo hàm nhập bệnh án

--- src/java/dal/AppointmentDAO.java
+++ src/java/dal/AppointmentDAO.java
@@ -16,13 +16,14 @@ public class AppointmentDAO extends DBContext {
     // Lấy danh sách lịch hẹn chờ khám hôm nay của bác sĩ
     public List<Appointment> getWaitingAppointmentToday(int doctorID) throws SQLException {
         List<Appointment> list = new ArrayList<>();
-        String sql = "SELECT a.AppoitmentID, a.CustomerID, a.DoctorID, a.AppointmentDate, a.AppointmentTime, a.Status, a.Notes "
-                + "FROM Appointments a"
-                + "JOIN Users u ON a.CustomerID = u.UserID"
-                + "WHERE a.AppointmentDate = CAST(GETDATE() AS DATE)"
-                + "AND a.Status IN ('Pending', 'Confirmed')"
+        String sql = "SELECT a.AppointmentID, a.CustomerID, a.DoctorID, a.AppointmentDate, a.AppointmentTime, a.Status, a.Notes, "
+                + "u.FullName AS PatientName, u.Phone AS PatientPhone "
+                + "FROM Appointments a "
+                + "JOIN Users u ON a.CustomerID = u.UserID "
+                + "WHERE a.AppointmentDate = CAST(GETDATE() AS DATE) "
+                + "AND a.Status IN ('Pending', 'Confirmed') "
                 + "AND (a.DoctorID = ? OR a.DoctorID IS NULL) "
-                + "ORDER BY a.AppointmentTime ACS";
+                + "ORDER BY a.AppointmentTime ASC";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, doctorID);
             try (ResultSet rs = ps.executeQuery()) {
@@ -40,7 +41,7 @@ public class AppointmentDAO extends DBContext {
                     list.add(a);
                 }
             }
-        } catch (SQLExceptionList e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return list;
@@ -57,5 +58,23 @@ public class AppointmentDAO extends DBContext {
             ex.printStackTrace();
         }
         return false;
+    }
+    
+        public int insertMedicalRecord(int appointmentID, int doctorID, String diagnosis, String treatmentPlan) throws SQLException {
+        String sql = "INSERT INTO MedicalRecords (AppointmentID, DoctorID, Diagnosis, TreatmentPlan, CreatedAt) "
+                + "VALUES (?, ?, ?, ?, GETDATE())";
+        try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, appointmentID);
+            ps.setInt(2, doctorID);
+            ps.setString(3, diagnosis);
+            ps.setString(4, treatmentPlan);
+            ps.executeUpdate();
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    return rs.getInt(1); // Trả về RecordID vừa tạo tự động
+                }
+            }
+        }
+        return -1;
     }
 }
